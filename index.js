52b60605fc68b69f6366dd4fd5fb9edce807ef57
Benjamin-brll <74306301+Benjamin-brll@users.noreply.github.com>
2021-01-31 15:31:01 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+document.getElementById('btn-enviar').addEventListener('click',(e)=>{
+    e.preventDefault()
+    createATask()
+})
+
+const createATask = () => {
+    let title = document.getElementById('title').value
+    let description = document.getElementById('description').value
+    let category = document.getElementById('category').value
+    let time = document.getElementById('time').value
+
+    let task = {
+        title,
+        description,
+        category,
+        time,
+    }
+    let timer = parseInt(time.split(':')[0])
+
+    if(timer > 22 || timer < 5){
+        Swal.fire({
+            title: `Es muy tarde para hacer algo`,
+            icon: 'info',
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: `Tienes razón`,
+            denyButtonText: `No me importa`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Buena decisión!', 'Ten una buena noche', 'success')
+              deleteTask(time)
+            } else if (result.isDenied) {
+              Swal.fire('Eres importante para nosotros', 'Por favor ve temprano a la cama', 'info')
+            }
+          })
+    }
+
+
+    if(localStorage.getItem('tasks')){
+        let tasks = JSON.parse(localStorage.getItem('tasks'))
+
+        tasks.push(task)
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }else{
+        let tasks = []
+        tasks.push(task)
+        
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+
+    showTasks()
+    document.getElementById('form').reset()
+}
+
+const deleteTask = (time) => {
+    let tasks = JSON.parse(localStorage.getItem('tasks'))
+
+    for(let i = 0; i < tasks.length; i++){
+
+        if(tasks[i].time == time){
+            tasks.splice(i,1)
+        }
+        
+    }
+    
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+    
+    showTasks()
+}
+
+const showTasks = () => {
+    let tasks = JSON.parse(localStorage.getItem('tasks'))
+    let lista = document.getElementById('lista')
+    lista.innerHTML = ""
+    
+    for(let i = 0; i < tasks.length; i++){
+        lista.innerHTML += `<div class="card mb-4">
+            <div class="card-body">
+                <h5 class="card-title">${tasks[i].title.toUpperCase()}</h5>
+                <p class="card-text">${tasks[i].description}</p>
+                <p class="card-text">${tasks[i].time}</p>
+                </div>
+            <div class="card-footer text-center">
+                <p class="font-weight-bold">Categoria:</p>
+                <p class="text-danger font-weight-bold">${tasks[i].category.toUpperCase()}</p>
+                <button class="btn btn-danger" onclick="deleteTask('${tasks[i].time}')">Delete</button>
+            </div>
+        </div>`
+    }
+}
+
+
+if(localStorage.getItem('tasks')){
+    showTasks()
+}
